4aa407dc88c5dcbfc548e4f878a49d443f7d3803
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-04-15 19:07:08 +1000

    Fixing empty colum add math.round in coin.price

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 var baseUrl = "https://api.coinranking.com/v2/coins"
 var proxyUrl = "https://cors-anywhere.herokuapp.com/"
-var apiKey= "coinranking9839ecbde6d8dd5e49f197db1308a5b152a9d46a9ab8d6db"
+var apiKey = "coinranking9839ecbde6d8dd5e49f197db1308a5b152a9d46a9ab8d6db"
 
-fetch(`${proxyUrl}${baseUrl}` , {
+fetch(`${proxyUrl}${baseUrl}`, {
   method: "GET",
   headers: {
-    'content-Type' : 'application/json',
+    'content-Type': 'application/json',
     'x.access-token': `${apiKey}`,
     'access-Control-Allow-Origin': '*'
-    } 
-  }).then((response)=>{
-    if (response.ok) {
-      response.json().then((json) => {
-        console.log(json.data.coins)
+  }
+})
+.then((response) => {
+  if (response.ok) {
+    response.json().then((json) => {
+      console.log(json.data);
+      let coinsData = json.data.coins;
 
-        let coinsData = json.data.coins
-        
-        if(coinsData.length > 0 ){
-          var cryptoCoins = ""
-        }
-
-          //for loop starts
-
-          coinsData.forEach((coins) => {
-            cryptoCoins += "<tr>"
-            cryptoCoins += `<td> ${coins.uuid} </td>`;
-            cryptoCoins += `<td> ${coins.btcPrice} <td>`;
-            cryptoCoins += `<td> ${coins.rank} </td>`;
-            cryptoCoins += `<td> ${coins.name} </td>`;
-            cryptoCoins += `<td> ${coins.price} </td>`;cryptoCoins += `<td> ${coins.symbol} </td>`; "<tr>"
-      
-          })
-                document.getElementById("data").innerHTML =cryptoCoins
-        
-      })
-    }
-    
-  }).catch ((error) =>{
-    console.log(error)
-  })
+      if (coinsData.length > 0) {
+        var cryptoCoin = "";
+      }
+      //For Loop Starts
+      coinsData.forEach((coin) => {
+        cryptoCoin += "<tr>";
+        cryptoCoin += `<td> ${coin.uuid} </td>`;
+        cryptoCoin += `<td> ${coin.btcPrice} </td>`;
+        cryptoCoin += `<td> ${coin.rank}</td>`;
+        cryptoCoin += `<td> ${coin.tier} </td>`;
+        cryptoCoin += `<td> ${coin.name}</td>`;
+        cryptoCoin += `<td> $${Math.round(coin.price)} Billion</td>`;
+        cryptoCoin += `<td> ${coin.symbol}</td>`;"<tr>";
+      });
+      //For Loop Ends
+      document.getElementById("data").innerHTML = cryptoCoin;
+    });
+  }
+})
+.catch((error) => {
+  console.log(error);
+});
