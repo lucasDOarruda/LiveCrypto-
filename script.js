57b9bac2b92bdabb1d9520f4b3de6d3f2fd96f60
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-04-15 15:45:41 +1000

    add script.js also fetching API handle responses / errors

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const options = {
-    headers: {
-      'Content-Type': 'application/json',
-       'x-access-token': 'coinranking9839ecbde6d8dd5e49f197db1308a5b152a9d46a9ab8d6db',
-    },
-  };
-  
-  fetch('https://api.coinranking.com/v2/coin/Qwsogvtv82FCd', options)
-    .then((response) => response.json())
-    .then((result) => console.log(result));
+var baseUrl1 = "https://api.coinranking.com/v2/coins"
+var proxyUrl1 = "https://cors-anywhere.herokuapp.com/"
+var apiKey= "0df19f02-168a-423b-825d-449113b05237"
+
+fetch(`${proxyUrl1}${baseUrl1}` , {
+  method: "GET",
+  headers: {
+    'content-Type' : 'application/json',
+    'x.access-token': `${apiKey}`,
+    'access-Control-Allow-Origin': '*'
+    } 
+  }).then((response)=>{
+    console.log(response)
+  }).catch ((error) =>{
+    console.log(error)
+  })
