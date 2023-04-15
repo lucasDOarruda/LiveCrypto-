8cb5b833a228bfb07ecd3b95ebb176cb6fd273a1
Lucas Miles <lucasmiles@Lucass-MacBook-Pro.local>
2023-04-15 20:25:42 +1000

    adding some backdround and aline h3 center

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var baseUrl = "https://api.coinranking.com/v2/coins"
+var baseUrl = "https://api.coinranking.com/v2/coins?limit=100"
 var proxyUrl = "https://cors-anywhere.herokuapp.com/"
 var apiKey = "coinranking9839ecbde6d8dd5e49f197db1308a5b152a9d46a9ab8d6db"
+
+
 
 fetch(`${proxyUrl}${baseUrl}`, {
   method: "GET",
